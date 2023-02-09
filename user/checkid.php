56e6ec3068a4955a1330f95b52835109876285df
Eunho <eunhokim98@naver.com>
2023-02-09 15:32:05 +0900

    sign_up 기능완료

--- user/checkid.php
+++ user/checkid.php
@@ -1,25 +1,16 @@
-<html>
-<head>
-</head>
-<body>
-    <h1>body</h1>
+
 <?php
     $conn = mysqli_connect("localhost", "root", "hacker98!", "web") or die ("Can't access DB");
     $id = $_GET['userid'];
-    
-    $sql = "SELECT * FROM user WHILE email=$id";
-    $result = mysqli_query($conn, $sql);
+    $sql = "SELECT email FROM user WHERE email='$id'";
+    $result = mysqli_query($conn, $sql) or die ("can't");
     $row = mysqli_fetch_array($result);
-
-    if(!$row[0]){
-        echo "email: $row[0]<br>";
-        echo "$id 사용가능";
+ 
+    if($row){
+        echo "X";
     }
     else{
-        echo "중복된 ID";
+        echo "O";
     }
 
-    
 ?>
-</body>
-</html>
--- user/register.php
+++ user/register.php
@@ -1,9 +1,3 @@
-<html>
-<head>
-<meta charset="utf-8">
-</head>
-<body>
-    <h1>hi</h1>
     <?php
     $conn = mysqli_connect("localhost", "root", "hacker98!", "web") or die ("Can't access DB");
 
@@ -18,18 +12,9 @@
     $insert_query = "INSERT INTO user(email, pw, nick_name, first_name, last_name, address) 
     VALUE ('$email', '$password', '$nickname', '$first_name','$last_name', '$address')";
     $result = mysqli_query($conn, $insert_query);
-    echo "3<br>";
-    if($result){
-      echo "success";
-    }
-    else{
-      echo "faild";
-      echo $conn->error;
-    }
-
-
+    echo "<script>alert('You are registered as a member.')</script>";
     mysqli_close($conn);
+    echo "<script>location.href='login.html'</script>";
   ?>
-</body>
-</html>
+
 
