2f84e9bba0f4285944ea06a5abe81a1dfba254b8
Eunho <eunhokim98@naver.com>
2023-02-11 17:53:48 +0900

    보드_내용_출력중

--- board/main_board.php
+++ board/main_board.php
@@ -180,12 +180,11 @@
         <input type="search" class="form-control" style="width: 85%;" placeholder="Search..." aria-label="Search">
       </div>
     </form>
-      
       <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
             <tr>
-              <th scope="col">number</th>
+              <th scope="col">index</th>
               <th scope="col">title</th>
               <th scope="col">writer</th>
               <th scope="col">Date</th>
@@ -194,6 +193,24 @@
             </tr>
           </thead>
           <tbody>
+          <?php
+            $conn = mysqli_connect("localhost", "root", "hacker98!", "web") or die ("Can't access DB");
+            $select = "SELECT * FROM article WHERE board = 'main'";
+            $result = mysqli_query($conn, $select) or die("!!!!!!!!!!!!!!!");
+            while($row = mysqli_fetch_array($result)){
+              echo "<tr>";
+              echo "<td>$row[index]</td>";
+              echo "<td>$row[title]</td>";
+              echo "<td>$row[email]</td>";
+              echo "<td>$row[date]</td>";
+              echo "<td>$row[hit]</td>";
+              echo "<td>$row[like]</td>";
+              echo "</tr>";
+            }
+
+            
+            
+          ?>
             <tr>
               <td>1,001</td>
               <td>random</td>
@@ -202,125 +219,7 @@
               <td>text</td>
               <td>hi</td>
             </tr>
-            <tr>
-              <td>1,002</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
-              <td>hi</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-              <td>hi</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>illustrative</td>
-              <td>hi</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>text</td>
-              <td>random</td>
-              <td>layout</td>
-              <td>dashboard</td>
-              <td>hi</td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>dashboard</td>
-              <td>irrelevant</td>
-              <td>text</td>
-              <td>hi</td>
-              <td>placeholder</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>hi</td>
-              <td>dashboard</td>
-              <td>illustrative</td>
-              <td>rich</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,007</td>
-              <td>placeholder</td>
-              <td>tabular</td>
-              <td>hi</td>
-              <td>information</td>
-              <td>irrelevant</td>
-            </tr>
-            <tr>
-              <td>1,008</td>
-              <td>random</td>
-              <td>hi</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-            <tr>
-              <td>1,009</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
-            </tr>
-            <tr>
-              <td>1,010</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>hi</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-            </tr>
-            <tr>
-              <td>1,011</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>hi</td>
-              <td>illustrative</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,012</td>
-              <td>text</td>
-              <td>placeholder</td>
-              <td>hi</td>
-              <td>layout</td>
-              <td>dashboard</td>
-            </tr>
-            <tr>
-              <td>1,013</td>
-              <td>dashboard</td>
-              <td>irrelevant</td>
-              <td>hi</td>
-              <td>text</td>
-              <td>visual</td>
-            </tr>
-            <tr>
-              <td>1,014</td>
-              <td>dashboard</td>
-              <td>illustrative</td>
-              <td>hi</td>
-              <td>rich</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,015</td>
-              <td>random</td>
-              <td>tabular</td>
-              <td>information</td>
-              <td>hi</td>
-              <td>text</td>
-            </tr>
+            
           </tbody>
         </table>
       </div>
