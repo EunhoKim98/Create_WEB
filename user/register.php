a92fa84cfcd2512a28e4a51ea46893b430980115
Eunho <eunhokim98@naver.com>
2023-02-18 11:25:31 +0900

    레지스터 오류

--- user/register.php
+++ user/register.php
@@ -15,18 +15,21 @@
   VALUE ('$email', '$password', '$nickname', '$first_name','$last_name', '$address', 0)";
   $result = mysqli_query($conn, $insert_query);
   echo "<script>alert('A verification code has been sent to the email. Please check.')</script>";
-
-
-  $confirm_code = uniqid();  
-  send_confirmation_email($email, $confirm_code);
-  $insert_code = "INSERT INTO confirmation_code(code, email) VALUES ('$confirm_code', '$email')";
-  mysqli_query($conn, $insert_code);
+  echo "<script>"
   mysqli_close($conn);
   echo "<script>location.href='check_code.html'</script>";
-
-  
-
-
 ?>
+  
+<script src="//code.jquery.com/jquery.min.js"></script>
+<script>
+  $a.jax({
+    url: "../mail.php",
+    type: "post";
+    data : {
+      email: $('#email').val();
+    }
+
+  })
+</script>
 
 
