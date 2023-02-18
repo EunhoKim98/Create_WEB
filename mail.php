4e3b3df73a886dc0730ca11b6a5466556cda8228
Eunho <eunhokim98@naver.com>
2023-02-18 12:23:11 +0900

    find_password 수정중

--- mail.php
+++ mail.php
@@ -2,16 +2,16 @@
   include('DB_config.php');
 // Function to send an email with a confirmation link
   $email = $_POST['email'];
-  $subject = "Email Confirmation";
   $confirm_code = uniqid();
+  $subject = "Email Confirmation";
   $message = "Please click the following link to confirm your email: ";
   $message .= "$confirm_code";
-  $headers = 'From: AITON_Board<eunhok98@gmail.com>\r\n';
-
+  $headers = 'From: webmaster<AITON_Board@gmail.com>\r\n';
+  
   // Send the email
-  mail($email, $subject, $message, $headers);
-
+  $result = mail($email, $subject, $message, $headers);
+  echo $result;
   $insert_code = "INSERT INTO confirmation_code(code, email) VALUES ('$confirm_code', '$email')";
   mysqli_query($conn, $insert_code);
 
-  
+?>
